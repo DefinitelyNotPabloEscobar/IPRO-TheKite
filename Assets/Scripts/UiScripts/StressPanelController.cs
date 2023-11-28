09d0877eb23a3825d3cfbee3c3c70ff4e550aa90
Hugo-Escobar <101110870+DefinitelyNotPabloEscobar@users.noreply.github.com>
2023-11-28 19:56:50 +0000

    Bug fix update

--- Assets/Scripts/UiScripts/StressPanelController.cs
+++ Assets/Scripts/UiScripts/StressPanelController.cs
@@ -3,20 +3,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using static UnityEngine.Rendering.DebugUI;
 
 public class StressPanelController : MonoBehaviour
 {
     public GameObject panel;
+    public Canvas canvas;
 
     private bool MoveUp = false;
     private bool MoveDown = false;
 
-    private float upPercentage = 0.5f;
-    private float downPercentage = -0.425f;
+    private float upPercentage = 0.2f;
+    private float downPercentage = -0.665f;
     private float upPosition;
     private float downPosition;
 
-    private float speed = 3500f;
+    private float speed = 3000f;
 
 
     private static float screenHeight;
@@ -24,21 +26,80 @@ public class StressPanelController : MonoBehaviour
 
     private void Start()
     {
+
         screenHeight = Screen.height;
         screenWidth = Screen.width;
 
+        Debug.Log("asdasasd " + screenWidth / screenHeight);
+
+        if (screenWidth/screenHeight < 1.7f)
+        {
+            speed = 1000f;
+            upPercentage = 0.4f;
+            downPercentage = -0.5f;
+        }
+        else if(screenWidth / screenHeight < 1.8f)
+        {
+            upPercentage = 0.36f;
+            downPercentage = -0.515f;
+        }
+        else if (screenWidth / screenHeight <= 2.05f)
+        {
+            upPercentage = 0.28f;
+            downPercentage = -0.6f;
+        }
+        else if (screenWidth / screenHeight <= 2.2f)
+        {
+            upPercentage = 0.26f;
+            downPercentage = -0.625f;
+        }
+
         upPosition = screenHeight * upPercentage;
         downPosition = screenHeight * downPercentage;
+        //RectTransform canvasRect = canvas.GetComponent<RectTransform>();
+
+        /*
+        upPosition = canvasRect.rect.height * upPercentage;
+        downPosition = canvasRect.rect.height * downPercentage;
+        */
+
+        /*
+        panel.anchoredPosition = new Vector2(
+                0,
+                downPosition);
+        */
 
         panel.transform.position = new Vector3(
-                screenWidth / 2,
-                downPosition,
-                panel.transform.position.z);
-
+            screenWidth / 2,
+            downPosition,
+            panel.transform.position.z);
     }
 
     private void Update()
     {
+        /*
+        if (MoveUp)
+        {
+            panel.anchoredPosition = new Vector2(0, panel.anchoredPosition.y + Time.deltaTime * speed);
+            if (panel.anchoredPosition.y >= upPosition)
+            {
+                MoveUp = false;
+                panel.anchoredPosition = new Vector2(0,upPosition);
+            }
+        }
+
+        else if (MoveDown)
+        {
+            panel.anchoredPosition = new Vector2(0, panel.anchoredPosition.y - Time.deltaTime * speed);
+            if (panel.anchoredPosition.y <= downPosition)
+            {
+                MoveDown = false;
+                panel.anchoredPosition = new Vector2(0, downPosition);
+
+            }
+        }
+        */
+
         if (MoveUp)
         {
             panel.transform.position = new Vector3(
@@ -97,6 +158,6 @@ public class StressPanelController : MonoBehaviour
 
     public bool isOnBottomHalf()
     {
-        return Util.IsWithinThreshold(panel.transform.position.y, downPosition, 1f);
+        return Util.IsWithinThreshold(panel.transform.position.y, downPosition, 10f);
     }
 }
